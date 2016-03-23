9205bb70d240df6cb5052f878cac79def47619cf
Simon Li <li@siutsin.com>
2016-03-23 16:37:06 +0800

    force get from specific chatId

--- controller/message.js
+++ controller/message.js
@@ -1,6 +1,7 @@
 var log = require('log-to-file-and-console-node');
 var Message = require('../model/message');
 var moment = require('moment');
+var _ = require('lodash');
 
 exports.addMessage = function (msg, callback) {
   var message = new Message();
@@ -14,23 +15,42 @@ exports.addMessage = function (msg, callback) {
   message.save(callback);
 };
 
-exports.getTopTen = function (callback) {
-  var sevenDaysAgo = moment().subtract(7, 'day').toISOString();
-  log.i(sevenDaysAgo);
-  Message.aggregate(
-    [
-      {
-        $match: {
-          dateCreated: {
-            $gte: new Date(sevenDaysAgo)
+exports.getTopTen = function (chatId, callback) {
+  var greaterThanOrEqualToSevenDaysQuery = {
+    chatId: chatId.toString(),
+    dateCreated: {
+      $gte: new Date(moment().subtract(7, 'day').toISOString())
+    }
+  };
+  Message.count(greaterThanOrEqualToSevenDaysQuery, function (err, total) {
+    log.i("Number of messages: " + total);
+    Message.aggregate(
+      [
+        {
+          $match: greaterThanOrEqualToSevenDaysQuery
+        },
+        {
+          $group: {
+            _id: '$userId',
+            username: {$first: '$username'},
+            firstName: {$first: '$firstName'},
+            lastName: {$first: '$lastName'},
+            count: {$sum: 1}
+          }
+        },
+        {
+          $sort: {
+            count: -1
           }
         }
-      },
-      {
-        $group: {
-          _id: '$userId',
-          count: {$sum: 1}
+      ], function (err, result) {
+        if (!err && result && _.isArray(result)) {
+          for (var i = 0, l = result.length; i < l; i++) {
+            result[i].total = total;
+            result[i].percent = ((result[i].count / total) * 100).toFixed(2) + '%';
+          }
         }
-      }
-    ], callback);
+        callback(err, result);
+      });
+  });
 };
