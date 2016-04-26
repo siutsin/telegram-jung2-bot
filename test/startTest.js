409b803a85bf87ac2497e735a7a7c362871e8b9d
Simon Li <li@siutsin.com>
2016-04-26 23:45:44 +0800

    add todo

--- test/startTest.js
+++ test/startTest.js
@@ -1,8 +1,10 @@
 var log = require('log-to-file-and-console-node');
 log.removeConsole();
+// express
+// TODO: add test case for app.js
 // route
 require('./route/testBotHandler');
-// TODO: add test case for route/root
+// TODO: add test case for route/root.js
 // model
 require('./model/testMessage');
 require('./model/testUsage');
