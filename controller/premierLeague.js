85a0030754fe35ef5599cbe720418b347d3280e7
Simon Li <li@siutsin.com>
2016-04-27 21:45:42 +0800

    add message for promotion and relegation #13

--- controller/premierLeague.js
+++ controller/premierLeague.js
@@ -27,7 +27,7 @@ var getTable = function () {
         }
       }])
     .sort('-count')
-    .limit(20);
+    .limit(Constants.CONFIG.PREMIER_LEAGUE_SIZE);
   query.exec(function (err, results) {
     if (err) {
       promise.error(err);
@@ -38,20 +38,79 @@ var getTable = function () {
   return promise;
 };
 
+var getCurrentGroupMessageCount = function (msg) {
+  var promise = new mongoose.Promise();
+  var chatId = msg.chat.id.toString();
+  Message.count({
+    chatId: chatId.toString(),
+    dateCreated: {
+      $gte: new Date(moment().subtract(7, 'day').toISOString())
+    }
+  }, function (err, total) {
+    if (err) {
+      promise.error(err);
+    } else {
+      promise.complete(total);
+    }
+  });
+  return promise;
+};
+
+var appendPromoteRelegateMessage = function (msg, leagueMessage, leagueTables, currentGroupMessageCount) {
+  const indexOfRelegationZoneStart = Constants.CONFIG.PREMIER_LEAGUE_SIZE - Constants.CONFIG.PREMIER_LEAGUE_RELEGATION_ZONE_SIZE; // 17
+  const indexOfRelegationZoneEnd = Constants.CONFIG.PREMIER_LEAGUE_SIZE - 1;
+  var chatId = msg.chat.id.toString();
+  var index = _.indexOf(leagueTables, _.find(leagueTables, function (group) {
+    return group._id === chatId;
+  }));
+  var title = msg.chat.title;
+  var numberOfMessageNeeded;
+  if (index >= indexOfRelegationZoneStart && index <= indexOfRelegationZoneEnd) { // 17-19
+    // relegation zone, show number of message needed in order to get rid of the relegation zone
+    var numberOfMessageInLastSafeGroup = leagueTables[indexOfRelegationZoneStart - 1].count;
+    numberOfMessageNeeded = (numberOfMessageInLastSafeGroup - currentGroupMessageCount) + 1;
+    leagueMessage = leagueMessage + '\n' + numberOfMessageNeeded + ' messages required for ' + title + ' to stay in 冗超聯...';
+  } else if (index >= 1 && index < indexOfRelegationZoneStart - 1) { // 1-16
+    // mid table, show number of message needed in order to takeover the next group
+    var titleOfPreviousGroup = leagueTables[index - 1].title;
+    var numberOfMessageInPreviousGroup = leagueTables[index - 1].count;
+    numberOfMessageNeeded = (numberOfMessageInPreviousGroup - currentGroupMessageCount) + 1;
+    leagueMessage = leagueMessage + '\n' + numberOfMessageNeeded + ' messages required for ' + title + ' to takeover ' + titleOfPreviousGroup;
+  } else if (index === 0 && leagueTables.length > 1) {
+    // champion with more than one groups in the league, show number of message more than first runner-up group
+    var titleOfFirstRunnerUpGroup = leagueTables[1].title;
+    var numberOfMessageInFirstRunnerUpGroup = leagueTables[1].count;
+    var numberOfMessageAbove = currentGroupMessageCount - numberOfMessageInFirstRunnerUpGroup;
+    leagueMessage = leagueMessage + '\n' + 'CHAMPION!!! ' + numberOfMessageAbove + ' messages above ' + titleOfFirstRunnerUpGroup;
+  } else if (index === 0 && leagueTables.length === 1) {
+    // champion with only one group in the league, do not show message
+    leagueMessage = leagueMessage + '\n';
+  } else {
+    // relegation zone, show number of message needed in order to get rid of the relegation zone
+    numberOfMessageNeeded = (_.last(leagueTables).count - currentGroupMessageCount) + 1;
+    leagueMessage = leagueMessage + '\n' + numberOfMessageNeeded + ' messages required for ' + title + ' to promote to 冗超聯...';
+  }
+  return leagueMessage;
+};
+
 var getTableMessage = function (msg) {
   var message = Constants.PREMIER_LEAGUE.TABLE_TITLE;
   return UsageController.isAllowCommand(msg).then(function onSuccess() {
     var promises = [
       UsageController.addUsage(msg),
-      getTable().then(function (results) {
-        for (var i = 0, l = results.length; i < l; i++) {
-          message += (i + 1) + '. ' + results[i].title + ' ' + results[i].count + '\n';
+      getTable().then(function (leagueTables) {
+        for (var i = 0, l = leagueTables.length; i < l; i++) {
+          message += (i + 1) + '. ' + leagueTables[i].title + ' ' + leagueTables[i].count + '\n';
         }
-        return message;
-      })
+        return {message: message, leagueTables: leagueTables};
+      }),
+      getCurrentGroupMessageCount(msg)
     ];
     return Promise.all(promises).then(function (results) {
-      var message = results[1];
+      var leagueMessage = results[1].message;
+      var leagueTables = results[1].leagueTables;
+      var currentGroupMessageCount = results[2];
+      message = appendPromoteRelegateMessage(msg, leagueMessage, leagueTables, currentGroupMessageCount);
       return message;
     });
   }, function onFailure(usage) {
