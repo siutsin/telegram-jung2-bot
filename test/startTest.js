5b923a99ddba823e27a057ed339931450e5a727f
Simon Li <li@siutsin.com>
2016-12-13 21:05:38 +0800

    update test db name

--- test/startTest.js
+++ test/startTest.js
@@ -2,7 +2,7 @@ require('codecov');
 var log = require('log-to-file-and-console-node');
 log.removeConsole();
 process.env.MESSAGE_CONTROLLER = 'mongoMessage';
-process.env.MONGODB_URL = '127.0.0.1:27017/jung2bottest';
+process.env.MONGODB_URL = '127.0.0.1:27017/jung2botTest';
 // express
 // TODO: add test case for app.js
 // route
