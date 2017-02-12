12920608e90655d532c124f6ad3fd513220efb4f
Simon Li <li@siutsin.com>
2017-02-12 23:49:37 +0800

    remove openshift, refactor www.js

--- src/bin/www.js
+++ src/bin/www.js
@@ -3,72 +3,40 @@
 /**
  * Module dependencies.
  */
+import '../env'
+import '@risingstack/trace'
+import app from '../app'
+import log from 'log-to-file-and-console-node'
+import http from 'http'
 
-require('dotenv').config()
-require('@risingstack/trace')
-var app = require('../app')
-var debug = require('debug')('telegram-bot:server')
-var http = require('http')
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = val => {
+  const port = parseInt(val, 10)
+  if (isNaN(port)) { return val }
+  if (port >= 0) { return port }
+  return false
+}
 
 /**
  * Get port from environment and store in Express.
  */
-
-var host = process.env.OPENSHIFT_NODEJS_IP
-var port = normalizePort(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || '3000')
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 
 /**
  * Create HTTP server.
  */
-
-var server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-if (!host) {
-  server.listen(port)
-} else {
-  server.listen(port, host)
-}
-server.on('error', onError)
-server.on('listening', onListening)
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort (val) {
-  var port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    // named pipe
-    return val
-  }
-
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
-}
+const server = http.createServer(app)
 
 /**
  * Event listener for HTTP server "error" event.
  */
 
-function onError (error) {
-  if (error.syscall !== 'listen') {
-    throw error
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port
-
+const onError = error => {
+  if (error.syscall !== 'listen') { throw error }
+  const bind = typeof port === 'string'  ? 'Pipe ' + port : 'Port ' + port
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
@@ -87,11 +55,15 @@ function onError (error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-
-function onListening () {
-  var addr = server.address()
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port
-  debug('Listening on ' + bind)
+const onListening = () => {
+  const address = server.address()
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + address.port
+  log.i('Listening on ' + bind)
 }
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
