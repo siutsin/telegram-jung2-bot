6cb8094581c262666313f2c349fe9cee852b8397
Simon Li <li@siutsin.com>
2017-03-06 00:34:46 +0800

    remove obsolete tests, update stress test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,42 +3,72 @@ import mocha from 'gulp-mocha'
 import runSequence from 'run-sequence'
 import istanbul from 'gulp-babel-istanbul'
 import standard from 'gulp-standard'
+import injectModules from 'gulp-inject-modules'
+import babel from 'gulp-babel'
+// import log from 'log-to-file-and-console-node'
 
+const src = 'src/**/*.js'
+const testFiles = 'test/**/*.js'
 const jsFiles = [
   '*.js',
-  'src/**/*.js',
-  'test/**/*.js'
+  src,
+  testFiles
 ]
 
 gulp.on('stop', () => process.nextTick(() => process.exit(0)))
 
-gulp.task('stress', () => gulp.src('./test/stress/testStress.js', {
-  read: false
-})
-  .pipe(mocha({reporter: 'spec'}))
-  .on('error', error => { throw error }))
+gulp.task('stress', () => gulp.src('./test/stress/testStress.js', {read: false})
+  .pipe(mocha({
+    reporter: 'spec',
+    timeout: 60000,
+    compilers: 'js:babel-core/register'
+  }))
+  .on('error', (e) => {
+    console.log(e.message)
+    process.exit(0)
+  }))
 
 gulp.task('standard', () => gulp.src(jsFiles)
   .pipe(standard())
   .pipe(standard.reporter('default', { breakOnError: true, quiet: false }))
 )
 
-gulp.task('preIstanbul', () => gulp.src(jsFiles)
-  .pipe(istanbul())
-  .pipe(istanbul.hookRequire()))
+// gulp.task('preIstanbul', () => gulp.src(jsFiles)
+//   .pipe(istanbul())
+//   .pipe(istanbul.hookRequire()))
+//
+// gulp.task('mochaAndIstanbul', () => gulp.src('./test/test.js', { read: false })
+//   .pipe(mocha({
+//     reporter: 'spec',
+//     timeout: 60000,
+//     compilers: 'js:babel-core/register'
+//   }))
+//   .pipe(istanbul.writeReports())
+//   .pipe(istanbul.enforceThresholds({ thresholds: { global: 0 } }))
+//   .on('error', () => process.exit(0)))
 
-gulp.task('mochaAndIstanbul', () => gulp.src('./test/test.js', { read: false })
-  .pipe(mocha({
-    reporter: 'spec',
-    timeout: 60000,
-    compilers: 'js:babel-core/register'
-  }))
-  .pipe(istanbul.writeReports())
-  .pipe(istanbul.enforceThresholds({ thresholds: { global: 0 } }))
-  .on('error', () => process.exit(0)))
+gulp.task('coverage', cb => {
+  gulp.src(src)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire()) // or you could use .pipe(injectModules())
+    .on('finish', () => {
+      gulp.src(testFiles)
+        .pipe(babel())
+        .pipe(injectModules())
+        .pipe(mocha({
+          reporter: 'spec',
+          timeout: 60000,
+          compilers: 'js:babel-core/register'
+        }))
+        .pipe(istanbul.writeReports())
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 0 } }))
+        .on('end', cb)
+    })
+})
 
 gulp.task('default', callback => runSequence(
   'standard',
-  'preIstanbul',
-  'mochaAndIstanbul',
+  'coverage',
+  // 'preIstanbul',
+  // 'mochaAndIstanbul',
   callback))
--- test/stress/testStress.js
+++ test/stress/testStress.js
@@ -1,2 +1,65 @@
-// Message
-require('./testStressMessage')
+import '../../src/env'
+import MessageController from '../../src/controller/message'
+
+const messageController = new MessageController()
+
+const sample = {
+  chat: {
+    id: 'stubChatId'
+  }
+}
+
+const repeat = async (n, recreatePromise) => {
+  let promises = []
+  for (let i = 0; i < n; i++) promises.push(recreatePromise)
+  return await Promise.all(promises)
+}
+
+describe('MessageStressTest', () => {
+
+  describe('getTopTen', function () {
+    it('test 1: once', async () => {
+      try {
+        await messageController.getTopTen(sample, true)
+      } catch (e) {
+        console.error(e.stack)
+      }
+    })
+
+    it('test 2: repeat 10 times', async () => {
+      try {
+        await repeat(10, messageController.getTopTen(sample, true))
+      } catch (e) {
+        console.error(e.stack)
+      }
+    })
+  })
+
+  describe('getAllJung', () => {
+    it('test 1: once', async () => {
+      try {
+        await messageController.getAllJung(sample, true)
+      } catch (e) {
+        console.error(e.stack)
+      }
+    })
+
+    it('test 2: repeat 10 times', async () => {
+      try {
+        await repeat(10, messageController.getAllJung(sample, true))
+      } catch (e) {
+        console.error(e.stack)
+      }
+    })
+  })
+
+  describe('simulating real usage', () => {
+    it('repeat 400 times', async () => {
+      try {
+        await repeat(400, messageController.getAllJung(sample, true))
+      } catch (e) {
+        console.error(e.stack)
+      }
+    })
+  })
+})
