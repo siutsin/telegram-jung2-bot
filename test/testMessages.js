4c714841f02e13df44a8caee80a57076061e9ea5
Simon Li <li@siutsin.com>
2019-04-13 04:16:47 +0800

    Improve testing (#47)
    100% test coverage

--- test/testMessages.js
+++ test/testMessages.js
@@ -1,12 +1,80 @@
 import test from 'ava'
 import path from 'path'
 import dotenv from 'dotenv'
+import AWS from 'aws-sdk-mock'
+import nock from 'nock'
 import Messages from '../src/messages'
+import stubEvent from './stub/onMessageEvent'
+import stubTopTenEvent from './stub/onTopTenMessageEvent'
+import stubAllJungEvent from './stub/onAllJungMessageEvent'
+import stubHelpEvent from './stub/onHelpMessageEvent'
+import stubEditMessageEvent from './stub/onEditMessageEvent'
+import stubAllJungMessageResponse from './stub/allJungMessageResponse'
+import stubAllJungDatabaseResponse from './stub/allJungDatabaseResponse'
 
 dotenv.config({ path: path.resolve(__dirname, '.env.testing') })
 
-const messages = new Messages()
+test.beforeEach(async t => {
+  AWS.mock('DynamoDB.DocumentClient', 'put', (params, callback) => {
+    callback(null, { Items: 'successfully query items from the database' })
+  })
+  AWS.mock('DynamoDB.DocumentClient', 'query', (params, callback) => {
+    callback(null, stubAllJungDatabaseResponse)
+  })
+})
 
-test.failing('newMessage', async t => {
-  t.is(messages.toString(), '')
+test.afterEach.always(async t => {
+  AWS.restore()
+  nock.restore()
+})
+
+test('newMessage', async t => {
+  const messages = new Messages()
+  const response = await messages.newMessage(stubEvent)
+  t.is(response.statusCode, 200)
+})
+
+test('newMessage - /junghelp', async t => {
+  nock(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`)
+    .post('/sendMessage')
+    .reply(200, {
+      data: stubAllJungMessageResponse
+    })
+  const messages = new Messages()
+  const response = await messages.newMessage(stubHelpEvent)
+  t.is(response.statusCode, 200)
+})
+
+test('newMessage - /topten', async t => {
+  nock(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`)
+    .post('/sendMessage')
+    .reply(200, {
+      data: stubAllJungMessageResponse
+    })
+  const messages = new Messages()
+  const response = await messages.newMessage(stubTopTenEvent)
+  t.is(response.statusCode, 200)
+})
+
+test('newMessage - /alljung', async t => {
+  nock(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`)
+    .post('/sendMessage')
+    .reply(200, {
+      data: stubAllJungMessageResponse
+    })
+  const messages = new Messages()
+  const response = await messages.newMessage(stubAllJungEvent)
+  t.is(response.statusCode, 200)
+})
+
+test('newMessage - edit_message', async t => {
+  const messages = new Messages()
+  const response = await messages.newMessage(stubEditMessageEvent)
+  t.is(response.statusCode, 204)
+})
+
+test('newMessage - error', async t => {
+  const messages = new Messages()
+  const response = await messages.newMessage({})
+  t.is(response.statusCode, 500)
 })
