0675233c916bcb0d04d267a4a3748d4aa246ad87
Simon Li <li@siutsin.com>
2019-04-13 22:13:33 +0800

    create a NAT instance to allow outgoing traffic for now

--- src/jung2botUtil.js
+++ src/jung2botUtil.js
@@ -1,31 +1,6 @@
 import axios from 'axios'
-import Pino from 'pino'
 
 export default class jung2botUtil {
-  constructor () {
-    this.logger = new Pino({ level: process.env.LOG_LEVEL })
-    const that = this
-    axios.defaults.headers.post['Content-Type'] = 'application/json'
-    axios.interceptors.request.use(config => {
-      // this.logger.debug('interceptors config.header', config.headers)
-      // this.logger.debug('interceptors config.data', config.data)
-      return config
-    }, error => {
-      that.logger.error('error', error)
-      return Promise.reject(error)
-    })
-    axios.interceptors.response.use(response => {
-      this.logger.debug('interceptors response.status', response.status)
-      this.logger.debug('interceptors response.header', response.headers)
-      this.logger.debug('interceptors response.data', response.data)
-      // this.logger.debug('response', response)
-      return response
-    }, error => {
-      that.logger.error('error', error)
-      return Promise.reject(error)
-    })
-  }
-
   async sendMessage (chatId, message) {
     // TODO: TELEGRAM_BOT_TOKEN should be loaded from SSM Parameter Store
     const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
@@ -33,17 +8,6 @@ export default class jung2botUtil {
       chat_id: chatId,
       text: message
     }
-    // this.logger.debug('data', data)
-    const response = await axios({
-      method: 'post',
-      responseType: 'json',
-      headers: { 'Content-Type': 'application/json' },
-      url,
-      data
-    })
-    // this.logger.debug('response.status', response.status)
-    // this.logger.debug('response.header', response.headers)
-    // this.logger.debug('response', response)
-    return response
+    return axios.post(url, data)
   }
 }
