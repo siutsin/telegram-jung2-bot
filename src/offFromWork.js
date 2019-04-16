65128989dd5dc6771537418a2af4812fd35a94d1
Simon Li <li@siutsin.com>
2019-04-16 12:39:41 +0800

    query by all group ids and throttle the request

--- src/offFromWork.js
+++ src/offFromWork.js
@@ -1,6 +1,7 @@
 import Pino from 'pino'
 import DynamoDB from './dynamodb'
 import Jung2botUtil from './jung2botUtil'
+import Statistics from './statistics'
 import pThrottle from 'p-throttle'
 
 const jung2botUtil = new Jung2botUtil()
@@ -8,10 +9,11 @@ const jung2botUtil = new Jung2botUtil()
 export default class OffFromWork {
   constructor () {
     this.dynamodb = new DynamoDB()
+    this.statistics = new Statistics()
     this.logger = new Pino({ level: process.env.LOG_LEVEL })
   }
 
-  async seperateByGroups (rows) {
+  async separateByGroups (rows) {
     const records = rows.reduce((soFar, row) => {
       if (!soFar[row.chatId]) {
         soFar[row.chatId] = []
@@ -32,12 +34,23 @@ export default class OffFromWork {
     }
   }
 
+  async statsPerGroup (groupIds, records) {
+    const GROUPS_PER_SECOND = 20
+    const throttled = pThrottle((id, report) => jung2botUtil.sendMessage(id, report), GROUPS_PER_SECOND, 1000)
+    for (const id of groupIds) {
+      const rawRowData = records[id]
+      const report = await this.statistics.generateReport(rawRowData)
+      await throttled(id, report)
+    }
+  }
+
   async off () {
     try {
       const rows = await this.dynamodb.getAllRowsWithinDays({ days: 7 })
-      const records = await this.seperateByGroups(rows)
+      const records = await this.separateByGroups(rows)
       const groupIds = Object.keys(records)
       await this.announcement(groupIds)
+      await this.statsPerGroup(groupIds, records)
       return true
     } catch (e) {
       this.logger.error(e.message)
