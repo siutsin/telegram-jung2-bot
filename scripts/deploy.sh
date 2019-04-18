be7c2378cd2ac63b893d90194788346a47a79320
Simon Li <li@siutsin.com>
2019-04-18 22:50:18 +0800

    generate .env instead

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ "${TRAVIS_BRANCH}" == "develop" ]]; then
-   export STAGE=STAGE_DEV
-   export REGION=REGION_DEV
-   export PROFILE=PROFILE_DEV
-   export MESSAGE_TABLE=MESSAGE_TABLE_DEV
-   export MESSAGE_TABLE_GSI=MESSAGE_TABLE_GSI_DEV
-   export DOMAIN=DOMAIN_DEV
-   export LOG_LEVEL=LOG_LEVEL_DEV
+   echo "STAGE=$STAGE_DEV" >> .env
+   echo "REGION=$REGION_DEV" >> .env
+   echo "PROFILE=$PROFILE_DEV" >> .env
+   echo "MESSAGE_TABLE=$MESSAGE_TABLE_DEV" >> .env
+   echo "MESSAGE_TABLE_GSI=$MESSAGE_TABLE_GSI_DEV" >> .env
+   echo "DOMAIN=$DOMAIN_DEV" >> .env
+   echo "LOG_LEVEL=$LOG_LEVEL_DEV" >> .env
 elif [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-   export STAGE=STAGE_PROD
-   export REGION=REGION_PROD
-   export PROFILE=PROFILE_PROD
-   export MESSAGE_TABLE=MESSAGE_TABLE_PROD
-   export MESSAGE_TABLE_GSI=MESSAGE_TABLE_GSI_PROD
-   export DOMAIN=DOMAIN_PROD
-   export LOG_LEVEL=LOG_LEVEL_PROD
+   echo "STAGE=$STAGE_PROD" >> .env
+   echo "REGION=$REGION_PROD" >> .env
+   echo "PROFILE=$PROFILE_PROD" >> .env
+   echo "MESSAGE_TABLE=$MESSAGE_TABLE_PROD" >> .env
+   echo "MESSAGE_TABLE_GSI=$MESSAGE_TABLE_GSI_PROD" >> .env
+   echo "DOMAIN=$DOMAIN_PROD" >> .env
+   echo "LOG_LEVEL=$LOG_LEVEL_PROD" >> .env
 else
    echo "Neither in develop or master branch - ${TRAVIS_BRANCH}"
    exit 1
@@ -24,5 +24,7 @@ fi
 sls deploy
 
 echo "Successful deployment for ${TRAVIS_BRANCH}"
+
+rm -f .env
 
 exit 0
