722167dd97680f9a81cb84d3fdc1150983ef939e
Simon Li <li@siutsin.com>
2019-04-19 04:08:43 +0800

    As first part of the migration, do not send message to telegram API during the migration, #76

--- src/jung2botUtil.js
+++ src/jung2botUtil.js
@@ -1,19 +1,20 @@
-import axios from 'axios'
-import Pino from 'pino'
+// import axios from 'axios'
+// import Pino from 'pino'
 
 export default class jung2botUtil {
-  constructor () {
-    this.logger = new Pino({ level: process.env.LOG_LEVEL })
-  }
+  // constructor () {
+  //   this.logger = new Pino({ level: process.env.LOG_LEVEL })
+  // }
   async sendMessage (chatId, message) {
     // TODO: TELEGRAM_BOT_TOKEN should be loaded from SSM Parameter Store
-    const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
-    const data = {
-      chat_id: chatId,
-      text: message
-    }
-    const response = axios.post(url, data)
-    this.logger.debug('response', response)
-    return response
+    // const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
+    // const data = {
+    //   chat_id: chatId,
+    //   text: message
+    // }
+    // const response = axios.post(url, data)
+    // this.logger.debug('response', response)
+    // return response
+    return true
   }
 }
