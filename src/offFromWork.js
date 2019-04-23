64e68eaa9402f676b592a0c445816f0238a53538
Simon Li <li@siutsin.com>
2019-04-24 00:53:49 +0800

    use bottleneck instead

--- src/offFromWork.js
+++ src/offFromWork.js
@@ -1,4 +1,5 @@
 import Pino from 'pino'
+import Bottleneck from 'bottleneck'
 import DynamoDB from './dynamodb'
 import Jung2botUtil from './jung2botUtil'
 import Statistics from './statistics'
@@ -24,14 +25,27 @@ export default class OffFromWork {
   }
 
   async statsPerGroup (groupIds, records) {
-    const promises = []
+    // https://core.telegram.org/bots/faq#my-bot-is-hitting-limits-how-do-i-avoid-this
+    // If you're sending bulk notifications to multiple users, the API will not allow more than 30 messages
+    // per second or so. Consider spreading out notifications over large intervals of 8—12 hours for best results.
+    // Also note that your bot will not be able to send more than 20 messages per minute to the same group.
+    const GROUPS_PER_MINUTE = 19 // testing
+    const PER_SECOND = 1000
+    const PER_MINUTE = 60 * PER_SECOND
+    const limiter = new Bottleneck({
+      reservoir: GROUPS_PER_MINUTE,
+      reservoirRefreshAmount: GROUPS_PER_MINUTE,
+      reservoirRefreshInterval: PER_MINUTE,
+      maxConcurrent: 10,
+      minTime: PER_SECOND
+    })
+    this.logger.debug('groupIds:', groupIds)
     for (const id of groupIds) {
       const rawRowData = records[id]
       let report = await this.statistics.generateReport(rawRowData, { limit: 10 })
       report = '夠鐘收工~~\n\n' + report
-      promises.push(this.jung2botUtil.sendMessage(id, report))
+      await limiter.schedule(() => this.jung2botUtil.sendMessage(id, report))
     }
-    await Promise.all(promises)
   }
 
   async off () {
