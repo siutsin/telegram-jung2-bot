b8227309b6d393130c4e4fbe168edff99c2cd783
Simon Li <li@siutsin.com>
2019-04-23 23:25:19 +0800

    remove throttle, combine offFromWork message into one, sort offFromWork broadcast by most active group.

--- src/offFromWork.js
+++ src/offFromWork.js
@@ -2,7 +2,6 @@ import Pino from 'pino'
 import DynamoDB from './dynamodb'
 import Jung2botUtil from './jung2botUtil'
 import Statistics from './statistics'
-import pThrottle from 'p-throttle'
 
 export default class OffFromWork {
   constructor () {
@@ -24,24 +23,13 @@ export default class OffFromWork {
     return records
   }
 
-  async announcement (groupIds) {
-    // https://core.telegram.org/bots/faq#my-bot-is-hitting-limits-how-do-i-avoid-this
-    const GROUPS_PER_SECOND = 20
-    const throttled = pThrottle(id => this.jung2botUtil.sendMessage(id, '夠鐘收工~~'), GROUPS_PER_SECOND, 1000)
-    for (const id of groupIds) {
-      this.logger.debug('announcement await throttled', id)
-      await throttled(id)
-    }
-  }
-
   async statsPerGroup (groupIds, records) {
-    const GROUPS_PER_SECOND = 20
-    const throttled = pThrottle((id, report) => this.jung2botUtil.sendMessage(id, report), GROUPS_PER_SECOND, 1000)
     for (const id of groupIds) {
       const rawRowData = records[id]
-      const report = await this.statistics.generateReport(rawRowData, { limit: 10 })
+      let report = await this.statistics.generateReport(rawRowData, { limit: 10 })
+      report = '夠鐘收工~~\n\n' + report
       this.logger.debug('statsPerGroup await throttled', id)
-      await throttled(id, report)
+      await this.jung2botUtil.sendMessage(id, report)
     }
   }
 
@@ -49,10 +37,13 @@ export default class OffFromWork {
     try {
       const rows = await this.dynamodb.getAllRowsWithinDays({ days: 7 })
       const records = await this.separateByGroups(rows)
-      const groupIds = Object.keys(records)
-      this.logger.debug('groupIds', groupIds)
-      await this.announcement(groupIds)
-      await this.statsPerGroup(groupIds, records)
+      // Message ordered by the most active groups
+      const orderedGroupIds = Object.keys(records)
+        .map(chatId => ({ chatId: chatId, count: records[chatId].length }))
+        .sort((a, b) => b.count - a.count)
+        .map(o => o.chatId)
+      this.logger.debug('orderedGroupIds', orderedGroupIds)
+      await this.statsPerGroup(orderedGroupIds, records)
       return true
     } catch (e) {
       this.logger.error(e.message)
