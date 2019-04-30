a09c454857ff6f5cf521739261e52cb5a2face56
Simon Li <li@siutsin.com>
2019-05-01 03:03:28 +0800

    update deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,11 @@
 if [[ "${TRAVIS_BRANCH}" == "develop" ]]; then
    echo "STAGE=$STAGE_DEV" >> .env
    echo "REGION=$REGION_DEV" >> .env
-   echo "MESSAGE_TABLE=$MESSAGE_TABLE_DEV" >> .env
-   echo "MESSAGE_TABLE_GSI=$MESSAGE_TABLE_GSI_DEV" >> .env
    echo "DOMAIN=$DOMAIN_DEV" >> .env
    echo "LOG_LEVEL=$LOG_LEVEL_DEV" >> .env
 elif [[ "${TRAVIS_BRANCH}" == "master" ]]; then
    echo "STAGE=$STAGE_PROD" >> .env
    echo "REGION=$REGION_PROD" >> .env
-   echo "MESSAGE_TABLE=$MESSAGE_TABLE_PROD" >> .env
-   echo "MESSAGE_TABLE_GSI=$MESSAGE_TABLE_GSI_PROD" >> .env
    echo "DOMAIN=$DOMAIN_PROD" >> .env
    echo "LOG_LEVEL=$LOG_LEVEL_PROD" >> .env
 else
