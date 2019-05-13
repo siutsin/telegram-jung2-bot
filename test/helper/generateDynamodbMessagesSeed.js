16969960bad5f9e61e2ba812b9197ac26345275f
Simon Li <li@siutsin.com>
2019-05-13 21:44:26 +0800

    Feature/optimise resource (#183)
    * adjust resource based on CloudWatch insight
    
    * enable x-ray tracking for queryEvent and offFromWork
    
    * fix local DynamoDB setting

--- test/helper/generateDynamodbMessagesSeed.js
+++ test/helper/generateDynamodbMessagesSeed.js
@@ -2,36 +2,32 @@
 
 const fs = require('fs')
 const path = require('path')
-const uuid = require('uuid')
 const moment = require('moment')
 
 const util = require('util')
 const writeFilePromise = util.promisify(fs.writeFile)
 const accessPromise = util.promisify(fs.access)
 
-const isMinify = true
-const fullPath = path.resolve(__dirname, 'dynamodbSeed.json')
+const isMinify = false
+const fullPath = path.resolve(__dirname, 'dynamodbMessagesSeed.json')
 
 const data = []
-const chatIds = [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]
-const userIds = [123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 7890, 12345, 23456]
+const chatIds = new Array(1000).fill(1).map((_, i) => i + 1)
+const userIds = new Array(10000).fill(1).map((_, i) => i + 1)
 
 const generate = async () => {
-  for (let i = 0; i < 10000; i++) {
-    const dateCreated = moment().utcOffset(8).subtract(i, 'minutes')
-    const ttl = moment().add(7, 'days')
+  for (let i = 0; i < 20000; i++) {
     const chatId = chatIds[Math.floor(Math.random() * chatIds.length)]
     const userId = userIds[Math.floor(Math.random() * userIds.length)]
     let record = {
       chatId: chatId,
-      chatTitle: `chatTitle ${chatId}`,
-      dateCreated: dateCreated.format(),
-      firstName: `firstName${userId}`,
-      lastName: `lastName${userId}`,
-      id: uuid.v4(),
-      ttl: ttl.format(),
+      chatTitle: `t${chatId}`,
+      dateCreated: moment().utcOffset(8).subtract(i, 'minutes').format(),
+      firstName: `f${userId}`,
+      lastName: `l${userId}`,
+      ttl: moment().add(7, 'days').unix(),
       userId: userId,
-      username: `username${userId}`
+      username: `u${userId}`
     }
     data.push(record)
   }
