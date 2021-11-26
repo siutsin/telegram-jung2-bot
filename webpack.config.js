98a67f036fb86db65bf2e7bea0f51263228d632f
Simon Li <li@siutsin.com>
2021-11-26 01:44:18 +0000

    clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,3 @@
-const path = require('path')
-
 module.exports = {
-  target: 'node',
-  mode: 'production',
-  entry: './src/fastify.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'commonjs2'
-  }
+  target: 'node'
 }
